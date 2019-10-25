ce5028e4d28b0edd73b45b477dd27be8b314d27e
Christian Volk <christian.a.volk@basf.com>
2019-10-25 07:36:36 +0200

    Support additional request parameters introduced in v1.9 + dev

--- projects/ng-oidc-client/src/lib/models/arguments.model.ts
+++ projects/ng-oidc-client/src/lib/models/arguments.model.ts
@@ -20,4 +20,7 @@ export interface RequestArugments {
   request?: any;
   request_uri?: string;
   extraQueryParams?: any;
+  extraTokenParams?: any; // https://github.com/IdentityModel/oidc-client-js/issues/745 
+  skipUserInfo?: boolean; // https://github.com/IdentityModel/oidc-client-js/issues/825
+  response_mode?: string; // https://github.com/IdentityModel/oidc-client-js/commit/5fbce7cfe529a706a99366bc5024c07dee024103#diff-daf6bf2d1b132f4a29f19d017b195b7c
 }
